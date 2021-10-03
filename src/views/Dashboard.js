a0cc99be6a05392d8a09f78a9e0b15187210aa1f
Tín <tinntse140668@fpt.edu.vn>
2021-10-03 12:58:52 +0700

    fix deploy 1

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -106,32 +106,7 @@ function Dashboard() {
               </Card.Footer> */}
             </Card>
           </Col>
-          {/* <Col lg="3" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-vector text-danger"></i>
-                    </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Errors</p>
-                      <Card.Title as="h4">23</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="far fa-clock-o mr-1"></i>
-                  In the last hour
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col> */}
+          
           <Col lg="3" sm="6">
             <Card className="card-stats">
               <Card.Body>
