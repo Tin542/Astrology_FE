369fe9c11ac1b70d44ae32f87b266753d264f3c4
Tín <tinntse140668@fpt.edu.vn>
2021-10-07 18:51:13 +0700

    add post

--- src/views/Posts/PostTable.js
+++ src/views/Posts/PostTable.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { del, post, get, put } from "../../service/ReadAPI";
+import Moment from 'react-moment';
+import moment from "moment";
 
 // react-bootstrap components
 import {
@@ -18,101 +20,71 @@ import {
   Tooltip,
 } from "react-bootstrap";
 
-export default class PostTables extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-      _posts: [],
-      pageList: [],
 
-      searchValue: "",
-      searchPageList: [],
-      isSearch: false,
-
-      // _categories: [],
-      // _publisher: [],
-      // _author: [],
-
-      // searchByCateValue: "",
-      // category_ID: "",
-      // searchByCatePageList: [],
-      // isSearchByCate: false,
-
-      page: 0,
-
-      id: 0,
-      title: "",
-      content: "",
-      created_at: "",
-      is_approve: false,
-      updated_at: "",
-      category_id: 0,
-      astrologer_id: 0,
-
-      // modalIsOpen: false,
-    };
-  }
-
-  componentDidMount() {
-    this.getData();
-  }
-
-  getData() {
-    const headers = {
-      "Content-Type": "application/json",
-      // Authorization: localStorage.getItem("auth"),
-    };
-    axios
-      .get("http://54.169.107.173/api/v1/posts", {
-        headers,
-      })
-      .then((response) => {
-        if (response.code === 0) {
-          this.setState({
-            loading: true,
-            // _products: response.data.data.Books,
-            _posts: response.data.list,
-          });
-          // this.showPageList(response.data);
-        }
+function PostTables() {
+  useEffect(() => {
+    getServiceList();
+    // displayFIeldName();
+    // displayStateName();
+    // get("​/api​/v1.0​/company​").then((res) => {
+    //   if (res && res.status === 200) {
+    //     setListFilterState(res.data);
+    //   }
+    // });
+    // get("/api​/v1.0​/major_field​").then((res) => {
+    //   if (res && res.status === 200) {
+    //     setListFilterState(res.data);
+    //   }
+    // });
+  }, []);
+  const [useListServiceShowPage, setUseListServiceShowPage] = useState([]);
+  function getServiceList() {
+    get("/api/v1/posts")
+      .then((res) => {
+        var temp = res.data.data.list;
+        console.log(temp);
+        // setName(temp.name);
+        // setDescription(temp.description);
+        // setImage(temp.picture);
+        // setIsDeleted(temp.is_Delete);
+        // setServiceList(temp);
+        setUseListServiceShowPage(temp);
+        // setUseListServiceShowPage(
+        //   temp.slice(numberPage * 5 - 5, numberPage * 5)
+        // );
+        // setTotalNumberPage(Math.ceil(temp.length / 5));
+        // setCount(count);
       })
       .catch((err) => {
-        alert(err.message);
+        console.log(err);
       });
   }
-
-  render() {
-    const { _posts, loading } = this.state;
-
-    if (!loading) {
-      return <h1>loading...</h1>;
-    }
-    return (
-      <>
-        <Container fluid>
-          <Row>
-            <Col md="12">
-              <Card className="table-big-boy">
-                <Card.Header>
-                  <Card.Title as="h4">All Post</Card.Title>
-                  <br></br>
-                </Card.Header>
-                <Card.Body className="table-full-width">
-                  <Table className="table-bigboy">
-                    <thead>
-                      <tr>
-                        <th className="text-center">Thumb</th>
-                        <th>Title</th>
-                        <th className="th-description">Description</th>
-                        <th className="text-right">Date</th>
-                        <th className="text-right">Status</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {_posts.map((item) => (
-                        <tr key={item.id}>
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col md="12">
+            <Card className="table-big-boy">
+              <Card.Header>
+                <Card.Title as="h4">All Post</Card.Title>
+                <br></br>
+              </Card.Header>
+              <Card.Body className="table-full-width">
+                <Table className="table-bigboy">
+                  <thead>
+                    <tr>
+                      <th className="text-center">Thumb</th>
+                      <th>Title</th>
+                      <th className="th-description">Description</th>
+                      <th className="text-right">Date</th>
+                      <th className="text-right">Status</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {useListServiceShowPage.map((item, index) => {
+                      return (
+                        <tr key={index}>
                           <td>
                             <div className="img-container">
                               <img
@@ -123,10 +95,8 @@ export default class PostTables extends Component {
                             </div>
                           </td>
                           <td className="td-name">{item.title}</td>
-                          <td>
-                            {item.content}
-                          </td>
-                          <td className="td-number">30/08/2016</td>
+                          <td>{item.content}</td>
+                          <td className="td-number">{moment(item.updated_at).format("MM-DD-YYYY")}</td>
                           <td className="td-number">Watting</td>
                           <td className="td-actions">
                             <OverlayTrigger
@@ -173,76 +143,18 @@ export default class PostTables extends Component {
                             </OverlayTrigger>
                           </td>
                         </tr>
-                      ))}
-                      <tr>
-                        <td>
-                          <div className="img-container">
-                            <img
-                              alt="..."
-                              src={
-                                require("assets/img/blog-1.jpg").default
-                              }></img>
-                          </div>
-                        </td>
-                        <td className="td-name">
-                          10 Things that all designers do
-                        </td>
-                        <td>
-                          Most beautiful agenda for the office, really nice
-                          paper and black cover. Most beautiful agenda for the
-                          office.
-                        </td>
-                        <td className="td-number">30/08/2016</td>
-                        <td className="td-number">Watting</td>
-                        <td className="td-actions">
-                          <OverlayTrigger
-                            overlay={
-                              <Tooltip id="tooltip-618009180">
-                                View Post..
-                              </Tooltip>
-                            }
-                            placement="left">
-                            <Button
-                              className="btn-link btn-icon"
-                              type="button"
-                              variant="info">
-                              <i className="far fa-image"></i>
-                            </Button>
-                          </OverlayTrigger>
-                          <OverlayTrigger
-                            overlay={
-                              <Tooltip id="tooltip-461494662">Approve</Tooltip>
-                            }
-                            placement="left">
-                            <Button
-                              className="btn-link btn-icon"
-                              type="button"
-                              variant="success">
-                              <i className="fas fa-edit"></i>
-                            </Button>
-                          </OverlayTrigger>
-                          <OverlayTrigger
-                            overlay={
-                              <Tooltip id="tooltip-408856985">Denined</Tooltip>
-                            }
-                            placement="left">
-                            <Button
-                              className="btn-link btn-icon"
-                              type="button"
-                              variant="danger">
-                              <i className="fas fa-times"></i>
-                            </Button>
-                          </OverlayTrigger>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </>
-    );
-  }
+                      );
+                    })}
+                    
+                   
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
+export default PostTables;
